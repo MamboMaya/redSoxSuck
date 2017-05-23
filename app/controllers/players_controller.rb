b474bb2420fe2c21bc8780adf329c9bdc0155e41
Damien Maya <damien_maya@yahoo.com>
2017-05-22 19:46:02 -0500

    added player functionality

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,27 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
   def show
+    @player = Player.find(params[:id])
   end
 
   def edit
+    @player = Player.find(params[:id])
   end
 
   def new
+    @team = Team.find(params[:team_id])
+    @player = @team.players.new
   end
+
+
+  private
+  def player_params
+    params.require(:player).permit(:name)
+  end
+
+
+
 end
